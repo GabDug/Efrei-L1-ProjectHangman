6670546bf17a2f7941efd00408859ddbcc74a0f1
vince1st <vincent.lacrouts@laposte.net>
2017-11-15 19:55:59 +0100

    adding the utf-8 encoding (bc of fenêtre)

--- Part 1/4 - Text Files/a.py
+++ Part 1/4 - Text Files/a.py
@@ -1,9 +1,10 @@
 # displays the number of words and the number of characters in the file
 
-with open("list.txt", "r") as rawfile:
+with open("list.txt", "r", encoding="utf-8") as rawfile:
     text = rawfile.read()
     words = text.split()
     text = text.replace('\n', '').replace('\r', '')
+    print(text)
     char = len(text.replace('\n', '').replace('\r', ''))
     print(f"Number of words : {len(words)}\n"
           f"Number of characters : {len(text)}")
